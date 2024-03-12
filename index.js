5db1fa51a0a3c4b8aeaabb3ffc165fd53aeccba6
Sageyuva <yuvarajdhammure@gmail.com>
2024-03-12 11:29:55 +0530

    update

--- index.js
+++ index.js
@@ -1,30 +1,26 @@
 const express = require("express");
 const dotenv = require("dotenv");
-const mongoose = require("mongoose");
+const { default: mongoose } = require("mongoose");
+const app = express();
 const cors = require("cors");
-const socketIO = require("socket.io");
 const { notFound, errorHandler } = require("./middleware/errorMiddleware");
 
-const app = express();
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 dotenv.config();
 
-// Middleware
-app.use(cors({ origin: "*" }));
 app.use(express.json());
 
-// Routes
 const userRoutes = require("./Routes/userRoutes");
 const chatRoutes = require("./Routes/chatRoutes");
 const messageRoutes = require("./Routes/messageRoutes");
 
-app.use("/user", userRoutes);
-app.use("/chat", chatRoutes);
-app.use("/message", messageRoutes);
-
-// Database Connection
 const connectDb = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
+    const connect = await mongoose.connect(process.env.MONGO_URI);
     console.log("Server is Connected to Database");
   } catch (err) {
     console.log("Server is NOT connected to Database", err.message);
@@ -32,46 +28,18 @@ const connectDb = async () => {
 };
 connectDb();
 
-// API Root
 app.get("/", (req, res) => {
   res.send("API is running123");
 });
+
+app.use("/user", userRoutes);
+app.use("/chat", chatRoutes);
+app.use("/message", messageRoutes);
 
 // Error Handling middlewares
 app.use(notFound);
 app.use(errorHandler);
 
-// Server Setup
 const PORT = process.env.PORT || 5000;
-const server = app.listen(PORT, () => {
-  console.log(`Server is Running on Port ${PORT}`);
-});
+app.listen(PORT, console.log("Server is Running..."));
 
-// Socket.io Setup
-const io = socketIO(server, {
-  cors: {
-    origin: "*",
-  },
-  pingTimeout: 6000,
-});
-
-io.on("connection", (socket) => {
-  socket.on("setup", (user) => {
-    socket.join(user.data._id);
-    socket.emit("connected");
-  });
-
-  socket.on("join chat", (room) => {
-    socket.join(room);
-  });
-
-  socket.on("new message", (newMessageStatus) => {
-    const chat = newMessageStatus.chat;
-    if (!chat.users) {
-      return console.log("chat user not defined");
-    }
-
-    // Assuming newMessageReceived is defined somewhere in your code
-    socket.in(user.id).emit("message received", newMessageReceived);
-  });
-});
